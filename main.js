5b9d0e10d8a942d0fa1b12fcb16b446257bb1b61
Tran Cong Tam <trancongtam550@gmail.com>
2023-11-30 18:07:12 +0700

    Add Label and Title

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-d3.csv("Data.csv").then(data => {
+d3.csv("Data.csv").then((data) => {
   // Parse the data
-  data.forEach(d => {
+  data.forEach((d) => {
     for (let i = 2010; i <= 2023; i++) {
       d[i] = +d[i];
     }
   });
 
   // Define the dimensions of the chart
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  const margin = { top: 30, right: 20, bottom: 60, left: 40 };
   const width = 960 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
   // Create the SVG container for the chart
-  const svg = d3.select("#chart").append("svg")
+  const svg = d3
+    .select("#chart")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -21,33 +23,69 @@ d3.csv("Data.csv").then(data => {
   // Create the scales
   const x = d3.scaleBand().range([-45, width]).padding(0.1);
   const y = d3.scaleLinear().range([height, 0]);
+  const x1 = d3.scaleLinear().range([0, width]);
 
   // Set the domains of the scales
-  x.domain(data.map(d => d["Unemployment rate (Percent)"]));
-  y.domain([0, d3.max(data, d => d[2023])]);
+  x.domain(data.map((d) => d["Unemployment rate (Percent)"]));
+  y.domain([0, d3.max(data, (d) => d[2023])]);
 
   // Add the x-axis
-  svg.append("g")
+  svg
+    .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
   // Add the y-axis
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  svg.append("g").call(d3.axisLeft(y));
 
+  //Create Title
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", height - 420)
+    .attr("text-anchor", "middle")
+    .style("font-family", "sans-serif")
+    .style("font-size", "20px")
+    .text("Unemployment Rate by Countries in 2023");
+
+  //Create Xlabel
+  svg
+    .append("text")
+    .attr("x", width - 30)
+    .attr("y", height + 40)
+    .attr("text-anchor", "middle")
+    .style("font-family", "sans-serif")
+    .style("font-size", "15px")
+    .text("Countries");
+  
+  //Create Ylabel
+  svg
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", -40)
+  .attr("x", -200)
+  .attr("dy", "1em")
+  .style("font-family", "sans-serif")
+  .style("font-size", "15px")
+  .text("Unemployment Rate (Percent)");
 
   // Create the color scale
-  const color = d3.scaleSequential()
-    .domain([0, d3.max(data, d => d[2023])])
+  const color = d3
+    .scaleSequential()
+    .domain([0, d3.max(data, (d) => d[2023])])
     .interpolator(d3.interpolateBlues);
   // Add the bars
-  svg.selectAll(".bar")
+  svg
+    .selectAll(".bar")
     .data(data)
-    .enter().append("rect")
+    .enter()
+    .append("rect")
     .attr("class", "bar")
-    .attr("x", d => x(d["Unemployment rate (Percent)"]))
+    .attr("x", (d) => x(d["Unemployment rate (Percent)"]))
     .attr("width", x.bandwidth())
-    .attr("y", d => y(d[2023]))
-    .attr("height", d => height - y(d[2023]))
-    .attr("fill", d => color(d[2023]));
+    .attr("y", (d) => y(d[2023]))
+    .attr("height", (d) => height - y(d[2023]))
+    .attr("fill", (d) => color(d[2023]));
+
+  // Add the lines chart
 });
