f069d9a1a788e14661725a6fdb35d187e3b71b5d
Tran Cong Tam <trancongtam550@gmail.com>
2024-01-05 19:29:25 +0700

    update linechart

--- linechart.js
+++ linechart.js
@@ -1,233 +1,97 @@
+var svg = d3.select("#chart"),
+  margin = { top: 40, right: 30, bottom: 40, left: 50 },
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-d3.csv("newdata.csv").then((data) => {
-  // Parse the data
-  data.forEach((d) => {
-    for (let i = 2010; i <= 2023; i++) {
-      d[i] = +d[i];
-    }
+var line = d3
+  .line()
+  .x(function (d) {
+    return x(d.year);
+  })
+  .y(function (d) {
+    return y(d.value);
   });
 
-  // Define the dimensions of the chart
-  const margin = { top: 30, right: 170, bottom: 60, left: 50 };
-  const width = 960 - margin.left - margin.right;
-  const height = 500 - margin.top - margin.bottom;
+svg
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Create the SVG container for the chart
-  const svg = d3
-    .select("#chart")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+d3.csv("newdata.csv").then(function (data) {
+  var keys = data.columns.slice(1);
+  var parseTime = d3.timeParse("%Y");
+  var countries = data
+    .map(function (d) {
+      return d["Unemployment rate (Percent)"];
+    })
+    .filter(function (d) {
+      return d;
+    });
 
-  // Create the scales
-  const x = d3.scaleLinear().range([0, width]);
-  const y = d3.scaleLinear().range([height, 0]);
-  const color = d3.scaleOrdinal(d3.schemeCategory20);
+  var select = d3
+    .select("#countrySelect")
+    .on("change", onchange)
+    .selectAll("option")
+    .data(countries)
+    .enter()
+    .append("option")
+    .text(function (d) {
+      return d;
+    });
 
-  // Set the domains of the scales
-  x.domain([2010, 2023]); // Assuming years from 2010 to 2023
-  y.domain([0, d3.max(data, (d) => d3.max(d3.values(d)))]);
+  function onchange() {
+    var selectedCountry = d3.select("#countrySelect").property("value");
+    updateChart(selectedCountry);
+  }
 
-  // Add the x-axis
-  svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+  function updateChart(country) {
+    var countryData = keys.map(function (year) {
+      return {
+        year: parseTime(year),
+        value: data.find(function (row) {
+          return row["Unemployment rate (Percent)"] === country;
+        })[year],
+      };
+    });
 
-  // Add the y-axis
-  svg.append("g").call(d3.axisLeft(y));
+    x.domain(
+      d3.extent(countryData, function (d) {
+        return d.year;
+      })
+    );
+    y.domain([
+      0,
+      d3.max(countryData, function (d) {
+        return d.value;
+      }),
+    ]);
 
-  function updateChart(selectedCountry) {
-    // Filter data to return only the selected country
-    let filteredData = data.filter(d => d["Country Name"] === selectedCountry);
-
-    // Remove the existing line (if any)
+    svg.selectAll(".axis").remove();
     svg.selectAll(".line").remove();
 
-    // Draw the line for the selected country
-    filteredData.forEach((countryData) => {
-      const countryValues = d3.range(2010, 2024).map(year => ({ year: year, rate: countryData[year] }));
-
-      svg
-        .append("path")
-        .datum(countryValues)
-        .attr("class", "line")
-        .attr("fill", "none")
-        .attr("stroke", "steelblue") // Or use color(i) if you have a color scale
-        .attr("stroke-width", 2)
-        .attr("d", line);
-    });
-  }
-
-
-  // Add Tooltip div
-  const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0)
-    .style("position", "absolute")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px");
-
-  // Define mouseover, mousemove, and mouseout functions
-  function mouseover(event, d) {
-    tooltip.transition().duration(200).style("opacity", .9);
-    d3.selectAll('.line').style('opacity', 0.1);
-    d3.select(this).style('stroke-width', '4').style('opacity', 1);
-  }
-
-  function mousemove(event, d, countryName) {
-    tooltip.html("Country: " + countryName + "<br>Year: " + d.year + "<br>Rate: " + d.rate + "%")
-      .style("left", (event.pageX) + "px")
-      .style("top", (event.pageY - 28) + "px");
-  }
-
-  function mouseout(event, d) {
-    tooltip.transition().duration(500).style("opacity", 0);
-    d3.selectAll('.line').style('stroke-width', '1.5').style('opacity', 1);
-  }
-
-  // Create the linechart
-  const line = d3
-    .line()
-    .x((d, i) => x(2010 + i)) // Use the index to calculate the year
-    .y((d) => y(d));
-
-  // Add the line
-  // Add the country name as label
-  data.forEach((countryData, i) => {
-    const countryValues = [];
-    for (let i = 2010; i <= 2023; i++) {
-      countryValues.push(countryData[i]);
-    }
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .attr("class", "axis axis--x")
+      .call(d3.axisBottom(x));
 
     svg
-      .append("path")
-      .datum(countryValues)
-      .attr("fill", "none")
-      .attr("stroke", color(i)) // Use the color scale here
-      .attr("stroke-width", 1.5)
-      .attr("d", line)
-
-
-    const maxCountryValue = d3.max(countryValues);
-
-    svg
+      .append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y))
       .append("text")
-      .attr("fill", color(i)) // Use the color scale here
-      .attr("stroke", "none")
-      .attr("x", x(2023) + 10) // Adjust the x position to be within the chart area
-      .attr("y", y(maxCountryValue)) // Position based on max value
-      .attr("dy", ".35em")
-      .attr("text-anchor", "start")
-      .text(countryData["Country Name"]);
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Rate %");
 
-    svg
-      .append("path")
-      .datum(countryValues)
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", color(i)) // Use the color scale here
-      .attr("stroke-width", 1.5)
-      .attr("d", line)
-      .on("mouseover", mouseover) // Add mouseover event
-      .on("mousemove", (event) => mousemove(event, countryData, countryData["Country Name"])) // Add mousemove event
-      .on("mouseout", mouseout); // Add mouseout event
-  });
-  //Create Title
-  svg
-    .append("text")
-    .attr("x", width / 2)
-    .attr("y", height - 385)
-    .attr("text-anchor", "middle")
-    .style("font-family", "sans-serif")
-    .style("font-size", "20px")
-    .text("Unemployment Rate by Countries in 2023");
+    svg.append("path").datum(countryData).attr("class", "line").attr("d", line);
+  }
 
-  //Create Xlabel
-  svg
-    .append("text")
-    .attr("x", width - 30)
-    .attr("y", height + 40)
-    .attr("text-anchor", "middle")
-    .style("font-family", "sans-serif")
-    .style("font-size", "15px")
-    .text("Year");
-
-
-  svg
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -40)
-    .attr("x", -200)
-    .attr("dy", "1em")
-    .style("font-family", "sans-serif")
-    .style("font-size", "15px")
-    .text("Unemployment Rate (Percent)");
-
-
-
-  const countryNames = data.map(d => d["Unemployment rate (Percent)"]);
-  color.domain(countryNames);
-
-
-
-  // Create a legend
-  // const legend = svg.selectAll(".legend")
-  //   .data(color.domain())
-  //   .enter().append("g")
-  //   .attr("class", "legend")
-  //   .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
-  // Add the legend with toggle functionality
-  const legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; })
-    .style("cursor", "pointer") // Add pointer on hover
-    .on("click", function (d) {
-      // Determine if current line is visible
-      var active = d.active ? false : true,
-        newOpacity = active ? 0 : 1;
-      // Hide or show the elements based on the ID
-      d3.select("#line" + d.replace(/ /g, "")).style("opacity", newOpacity);
-      // Update whether or not the elements are active
-      d.active = active;
-    });
-
-
-
-  // Draw legend colored rectangles
-  legend.append("rect")
-    .filter(function (d, i) { return i !== 0; }) // Exclude the first element
-    .attr("x", width + margin.right - 150) // Adjust this value to move the rectangles outside of the chart window
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
-
-  // Draw legend text with countries name
-  legend.append("text")
-    .attr("x", width - 18 + 65) // Adjust this value to place the text to the right of the rectangles
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "start") // Change this to "start" to left-align the text
-    .style("font-family", "sans-serif")
-    .style("font-size", "12px")
-    .text(function (d) { return d; })
-    .attr("fill", "black");
-
-  // Add IDs to lines and legend items for toggle functionality
-  svg.selectAll(".line").attr("id", function (d) {
-    return "line" + d["Country Name"].replace(/ /g, "");
-  });
-  legend.append("text").attr("id", function (d) {
-    return "legend" + d.replace(/ /g, "");
-  });
-
-
+  updateChart(countries[0]);
 });
